4756e9ca238cd489f0c04551632abdcce7b0c142
Shane Xie <shane369@gmail.com>
2015-01-13 08:48:16 -0600

    startup

---
+++ src/src/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from calTrack import views as ct_views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'src.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$',ct_views.index),
+)
