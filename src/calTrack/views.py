f282cd6a64f43128325eca37b8fe4c1f2935e4d6
Nitesh Singhania <niteshsinghania@wpa-7-498.cc.umanitoba.ca>
2015-02-02 12:40:27 -0600

    changed httpresponse back

--- src/calTrack/views.py
+++ src/calTrack/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext, Context, loader
 from django.views import generic
+from django.http import HttpResponse
 from calTrack.calTrackModels.foods import Foods
 
 # Create your views here.
