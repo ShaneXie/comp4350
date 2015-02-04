38dff730e16152540ca2d54580587fd87fcb9698
niteshsinghania <niteshsinghania@hotmail.com>
2015-02-03 21:02:37 -0600

    Added basic structure for validations and testing.

--- src/calTrack/calTrackModels/foods.py
+++ src/calTrack/calTrackModels/foods.py
@@ -1,12 +1,32 @@
 from django.db import models
+from django.core.validators import ValidationError
+
 
 # Create your models here.
+
+FOOD_TYPE_CHOICES = (
+    ('l','Lunch'),
+    ('d','Dinner'),
+    ('b','Breakfast'),
+    ('s','Snacks'),
+)
 
 class Foods(models.Model):
 
     fName = models.CharField(max_length = 50)
-    fCalorie = models.IntegerField (default=0)
-    fType = models.CharField (max_length = 1, blank = True)
+    fCalorie = models.PositiveIntegerField (default=1)
+    fType = models.CharField (choices=FOOD_TYPE_CHOICES, max_length = 1)
+
+    def getFoodListByType(self, foodType):
+
+    	foodList = Foods.objects.filter(fType__exact = 'foodType')
+    	return foodList
 
     def __unicode__(self):
-        return "{0} {1} {2}".format(self.fName, self.fCalorie, self.fType)
+        return self.fName
+
+    def clean(self):
+
+    	if self.fCalorie == 0:
+    		raise ValidationError({'fCalorie': 'Food calories cannot be zero.'})
+
