c7fd9bc0ee31c115577762ff7d591d56a5076344
Shane Xie <shane369@gmail.com>
2015-02-07 03:19:11 -0600

    add function for add new user to system

--- src/calTrack/dataAccess.py
+++ src/calTrack/dataAccess.py
@@ -1,6 +1,7 @@
 __author__ = 'An'
 
-from calTrack.models import Foods
+from calTrack.models import Foods,UserProfile
+from django.contrib.auth.models import User
 
 
 def getAllFoods():
@@ -11,5 +12,24 @@ def login(post):
     return True
 
 def add_user(post):
+    email = post['regEmailName']
+    username = email
+    pwd = post['regPwdName']
+    fName = post['regFirstName']
+    lName = post['regLastName']
+    height = post['regHgtName']
+    weight = post['regWgtName']
+    gender = post['genName']
+
+    print "line 0"
+    user = User.objects.create_user(username, email, pwd)
+    #user = User(username=username,email=email,password=pwd,first_name=fName,last_name=lName)
+    user.first_name = fName
+    user.last_name = lName
+    user.save()
+    print "line 1"
+    profile = UserProfile(user=user,weight=weight,height=height,gender=gender)
+    profile.save()
+
     #todo add new user
     return True
