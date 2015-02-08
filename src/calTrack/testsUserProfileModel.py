1001be4f61da47bb4173d202c2cd3cdcdcd8e855
niteshsinghania <niteshsinghania@hotmail.com>
2015-02-08 11:39:47 -0600

    Fixed a test case for Food model

--- src/calTrack/testsUserProfileModel.py
+++ src/calTrack/testsUserProfileModel.py
@@ -1,3 +1,16 @@
 from django.utils import unittest
 from calTrack.models import UserProfile
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+
+
+class UserProfileTestCase(unittest.TestCase):
+
+	def testUserProfileCreation(self):
+
+		userProfileTest = UserProfile(user = User(username="Profile Test", password = "hello", first_name = "Fname", last_name = "Lname", email = "ProfileTest@test.com"), age = "20", weight = "60", height = "124", gender = "m" )
+		self.assertTrue(isinstance(userProfileTest, UserProfile))
+		self.assertEqual(userProfileTest.__unicode__(),' '.join([
+            userProfileTest.user.first_name,
+            userProfileTest.user.last_name,
+        ]))
