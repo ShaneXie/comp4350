ccf59cde3d45bb8a5dfa58dafb72f8ff0ae46d5e
niteshsinghania <niteshsinghania@hotmail.com>
2015-03-01 17:19:31 -0600

    user profile page implementation_2

--- src/calTrack/api.py
+++ src/calTrack/api.py
@@ -1,7 +1,8 @@
-__author__ = 'anx'
+__author__ = 'anx and Nitesh'
 
 
 from calTrack.models import Foods,UserProfile
+from django.contrib.auth.models import User
 from django.core import serializers
 import json
 
@@ -9,3 +10,11 @@ import json
 def allFoodsJson():
     raw_data = serializers.serialize("json", Foods.objects.all())
     return raw_data
+
+
+
+def userProfile(req):
+	username = req.user
+	profile =  UserProfile.objects.filter(user__username=username)
+	raw_data = serializers.serialize("json", profile)
+	return raw_data
