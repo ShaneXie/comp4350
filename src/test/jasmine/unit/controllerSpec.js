d4994292b0b7844277e111d89f212e1ac15e1828
niteshsinghania <niteshsinghania@hotmail.com>
2015-03-08 15:43:15 -0500

    Reorganized files and start client side testing with jasmine

---
+++ src/test/jasmine/unit/controllerSpec.js
@@ -0,0 +1,28 @@
+describe("true",function(){
+	it("should be true", function(){
+		expect(true).toBeTruthy();
+	});
+
+});
+
+describe("NavController",function(){
+    var $rootScope,
+        $scope,
+        controller;
+    beforeEach(function () {
+        module('cts');
+
+        inject(function($injector) {
+            $rootScope = $injector.get('$rootScope');
+            $scope = $rootScope.$new();
+            controller = $injector.get('$controller')("NavController",{$scope: $scope});
+        });
+    });
+
+    describe("Initialization", function(){
+       it("Should instantiate item to 1", function(){
+           expect($scope.item).toEqual(1);
+       }) ;
+    });
+
+});
