a7becf775e900c50d4c52b3d37195e03c233df80
niteshsinghania <niteshsinghania@hotmail.com>
2015-03-09 22:44:03 -0500

    Split ctsapp complete.

--- src/static/js/ctsapp/ctsapp.js
+++ src/static/js/ctsapp/ctsapp.js
@@ -9,50 +9,6 @@
   }]);
 
 
-    app.controller('loginController', ['$http', '$scope' ,'$cookies',function($http,$cookies,$scope){
-        $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-        $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
-
-        $scope.loginInfo={};
-        $scope.regInfo = {};
-
-        $scope.login = function (){
-          $scope.loginInfo.csrfmiddlewaretoken = $cookies.csrftoken;
-          data=jQuery.param($scope.loginInfo);
-          console.log($scope.loginInfo);
-          $http.post('/ajax/login/',data).success(function (response) {
-            if(response=="success"){
-                 alert("Welcome Back!");
-                //update csrf token
-                $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
-                 location.reload();
-             }else{
-                 alert(response);
-             }
-
-          });
-      };
-
-      $scope.reg = function (){
-          $scope.regInfo.csrfmiddlewaretoken = $cookies.csrftoken;
-          data=jQuery.param($scope.regInfo);
-          console.log(data);
-          $http.post('/ajax/register/',data).success(function (response) {
-            if(response=="success"){
-                alert("Welcome to CTS!");
-                //update csrf token
-                $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
-                location.reload();
-             }else{
-                 alert(response);
-             }
-
-          });
-      };
-
-
-    }]);
-
 
   
 
