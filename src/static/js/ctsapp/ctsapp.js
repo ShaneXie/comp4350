1d23de2f681c861e5a9c63ca7c54974ba897d99d
niteshsinghania <niteshsinghania@hotmail.com>
2015-03-09 21:38:15 -0500

    Login and Register submit needs to be fixed.

--- src/static/js/ctsapp/ctsapp.js
+++ src/static/js/ctsapp/ctsapp.js
@@ -9,16 +9,17 @@
   }]);
 
 
-    app.controller('loginController', ['$http', '$cookies',function($http,$cookies){
+    app.controller('loginController', ['$http', '$scope' ,'$cookies',function($http,$cookies,$scope){
         $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
         $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
 
-        this.loginInfo={};
-        this.regInfo = {};
+        $scope.loginInfo={};
+        $scope.regInfo = {};
 
-        this.login = function (){
-          this.loginInfo.csrfmiddlewaretoken = $cookies.csrftoken;
-          data=jQuery.param(this.loginInfo);
+        $scope.login = function (){
+          $scope.loginInfo.csrfmiddlewaretoken = $cookies.csrftoken;
+          data=jQuery.param($scope.loginInfo);
+          console.log($scope.loginInfo);
           $http.post('/ajax/login/',data).success(function (response) {
             if(response=="success"){
                  alert("Welcome Back!");
@@ -32,9 +33,9 @@
           });
       };
 
-      this.reg = function (){
-          this.regInfo.csrfmiddlewaretoken = $cookies.csrftoken;
-          data=jQuery.param(this.regInfo);
+      $scope.reg = function (){
+          $scope.regInfo.csrfmiddlewaretoken = $cookies.csrftoken;
+          data=jQuery.param($scope.regInfo);
           console.log(data);
           $http.post('/ajax/register/',data).success(function (response) {
             if(response=="success"){
@@ -54,42 +55,5 @@
 
 
   
-
-  app.controller('bmiController', function(){
-    this.isStd = false;
-
-    this.stdHgt;
-    this.stdWgt;
-    this.mtrHgt;
-    this.mtrWgt;
-    this.bmi;
-
-    this.result = "Your BMI is ";
-    this.resultReady = false;
-
-    this.calc = function(){
-      this.result = "Your BMI is ";
-      if (this.isStd) {
-        this.bmi = (this.stdWgt/(this.stdHgt*this.stdHgt))*703;
-      } else {
-        this.bmi = (this.mtrWgt/(this.mtrHgt*this.mtrHgt))*10000;
-      }
-      var msg="";
-      this.result+=(Math.round(this.bmi * 100) / 100+" You are ");
-      if(this.bmi<=18.5){
-          msg = "Underweight ";
-      }else if(this.bmi>18.5&&this.bmi<25){
-          msg = "Normal weight";
-      }else if(this.bmi>=25&&this.bmi<=29.9){
-          msg = "Overweight";
-      }else{
-          msg = "Obesity";
-      }
-      this.result+=msg;
-      this.resultReady = true;
-    };
-
-  });
-
 
 
