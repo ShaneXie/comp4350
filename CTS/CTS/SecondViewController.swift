88202e567e7d933d70c23a63622296d5aa1f8df9
ShaneXie <shane369@gmail.com>
2015-03-17 00:22:05 -0500

    iOS init

---
+++ CTS/CTS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CTS
+//
+//  Created by Shane on 2015-03-14.
+//  Copyright (c) 2015 Comp4350Team7. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
