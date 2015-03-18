376cb8c767a1953889f0334aee85859687109c83
ark211 <donotreallyexist@gmail.com>
2015-03-18 00:34:36 -0500

    filename fix

--- CTS/CTS/BMIViewController.swift
+++ CTS/CTS/BMIViewController.swift
@@ -1,19 +1,18 @@
 //
-//  BMIViewController.swift
+//  SecondViewController.swift
 //  CTS
 //
-//  Created by Chuck on 2015-03-17.
+//  Created by Shane on 2015-03-14.
 //  Copyright (c) 2015 Comp4350Team7. All rights reserved.
 //
 
 import UIKit
 
-class BMIViewController: UIViewController {
+class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,14 +21,6 @@ class BMIViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
