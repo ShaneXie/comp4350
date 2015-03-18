45673180108a8b25984633f7b550c6f99b4aced7
ark211 <chuck@Chucks-Mac-2.local>
2015-03-17 21:02:06 -0700

    SMI
    @ShaneXie

---
+++ CTS/CTS/BMIViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BMIViewController.swift
+//  CTS
+//
+//  Created by Chuck on 2015-03-17.
+//  Copyright (c) 2015 Comp4350Team7. All rights reserved.
+//
+
+import UIKit
+
+class BMIViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
