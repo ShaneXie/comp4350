9aa3961e09a0c539e39736c4bb4a5fb86dd1a32d
ark211 <donotreallyexist@gmail.com>
2015-03-26 14:15:14 -0500

    temp save, not done yet

---
+++ CTS/CTS/LoginViewController.swift
@@ -0,0 +1,47 @@
+//
+//  LoginViewController.swift
+//  CTS
+//
+//  Created by Chuck on 2015-03-26.
+//  Copyright (c) 2015 Comp4350Team7. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var account: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var login: UIButton!
+    @IBOutlet weak var register: UIButton!
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
